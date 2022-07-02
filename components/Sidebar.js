5de7cd0c7f0531b0b8a2d2cb57b99465794b1186
Maaz-Code <500077097@stu.upes.ac.in>
2022-07-02 13:38:42 +0530

    Added routing to individual chat pages and display chats on sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,22 +4,35 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from "../firebaseconfig";
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { collection } from "firebase/firestore";
-
+import getOtherEmail from "../utils/getOtherEmail";
+import {useRouter} from "next/router";
 
 const Sidebar = () => {
     const [user] = useAuthState(auth);
     const [snapshot, loading, error] = useCollection(collection(db, 'chats'));
-    const chats = snapshot?.docs.map(docs => ({id: docs.id, ...docs.data()}));
+    const chats = snapshot?.docs.map(doc => ({id: doc.id, ...doc.data()}));
+    const router = useRouter();
+    
+    const redirect = (id) => {
+        router.push(`/chat/${id}`);
+    }
 
     const chatList = () => {
         return (
-            chats?.map ( chat => (
-                <Flex key = {Math.random()} align = "center" p = {3} _hover = {{bg: "gray.200", cursor: "pointer"}}>
-                    <Avatar src = "" marginEnd = {3}/>
-                    <Text>{chat.users}</Text>
-                </Flex>
-            ))
+            chats?.filter(chat => chat.users.includes(user.email))
+            .map ( 
+                chat => 
+                    <Flex key = {Math.random()} align = "center" p = {3} _hover = {{bg: "gray.200", cursor: "pointer"}} 
+                    onClick = {() => redirect(chat.id)}>
+                        <Avatar src = "" marginEnd = {3}/>
+                        <Text>{getOtherEmail(chat.users, user)}</Text>
+                    </Flex>
+            )
         )
+    }
+
+    const newChat = () => {
+        router.push(`/newChat`);
     }
 
     return (
@@ -43,7 +56,10 @@ const Sidebar = () => {
                 <IconButton isRound icon = {<CloseIcon/>} size = "sm" color = "#433491"/>
             </Flex>
 
-            <Button bg = "blue.50" color = "blue.500" m = {5} p = {4} _hover = {{bg: "blue.100", cursor: "pointer"}}>New Chat</Button>
+            <Button bg = "blue.50" color = "blue.500" m = {5} p = {4} _hover = {{bg: "blue.100", cursor: "pointer"}}
+            onClick = {() => newChat()}>
+                New Chat
+            </Button>
 
             <Flex overflowY = "auto" direction = "column" sx = {{scrollbarWidth: "none"}} flex = {1}>
                 {chatList()}
